13dde6bb3cf5dcc4814329494115aec8ff7741ed
codesenju <codesenju@gmail.com>
2020-05-27 19:58:01 +0200

    Discover kernel release using the Ad-Hoc command module

--- ad-hoc.sh
+++ ad-hoc.sh
@@ -4,7 +4,10 @@
 #ansible -i webapp web -m debug -a "msg='shutdown -r now'"
 
 # queries your fleet to see which version of ansible is installed
-ansible -i webapp web -m command -a "rpm -q ansible"
+#ansible -i webapp web -m command -a "rpm -q ansible"
 
 # distributes a file - critfile to the hosts
-ansible -i webapp web -m copy -a "src=critfile dest=/tmp/critfile"
+#ansible -i webapp web -m copy -a "src=critfile dest=/tmp/critfile"
+
+# discover the kernel release on the local machine using the command module
+ansible localhost -m command -a "uname -r"
